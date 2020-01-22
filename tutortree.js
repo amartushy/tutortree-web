e2480d4445bcf0d72eec556a95f18ea820e0a524
amartushy <adrian@jointutortree.com>
2020-01-21 22:54:00 -0800

    testing upcoming

--- tutortree.js
+++ tutortree.js
@@ -83,42 +83,6 @@ firebase.auth().onAuthStateChanged(function(user) {
    
       }
     })
- 
-    //LOAD UPCOMING SESSIONS
-    dataRef.once("value", function(snapshot) {
-    	var currentTime = Math.round((new Date()).getTime() / 1000);
-      var hasUpcoming = false
-      var upcomingSection = document.getElementById('upcoming-section')
-
-      for (sessionId in snapshot.child(userId+"/sessions").val()) {
-				if(snapshot.child(userId+"/sessions/"+sessionId+"/start").val() > currentTime){
-        	hasUpcoming = true
-          var upcomingBlock = document.createElement("div");
-          var upcomingStudent = document.createElement("h2")
-          var upcomingCourse = document.createElement("button")
-          var upcomingDate = document.createElement("h3")
-          var studentId = snapshot.child(userId+"/sessions/"+sessionId+'/other').val()
-          var startTime = snapshot.child(userId+'/sessions/'+sessionId+'/course/start').val()
-          var endTime = snapshot.child(userId+'/sessions/'+sessionId+'/course/end').val()
-          upcomingCourse.setAttribute('class', 'course-button w-button')
-          upcomingBlock.setAttribute('class', 'upcoming-block')
-          upcomingStudent.setAttribute('class', 'upcoming-header')
-          upcomingDate.setAttribute('class', 'upcoming-date')
-          
-          upcomingStudent.innerHTML = snapshot.child(studentId+'/name/').val()
-          upcomingCourse.innerHTML = snapshot.child(userId+'/sessions/'+sessionId+'/course').val()
-          upcomingDate.innerHTML = startTime + endTime
-          upcomingBlock.appendChild(upcomingStudent)
-          upcomingBlock.appendChild(upcomingCourse)
-          upcomingBlock.appendChild(upcomingDate)
-          upcomingSection.appendChild(upcomingBlock)
-         
-        }
-      }
-      if (hasUpcoming == false) {
-        	upcomingSection.innerHTML = "No upcoming sessions"
-        }
-    })
 
     //LOAD PROFILE
 		ref.on("value", function(snapshot) {
