5147b5b6bceda3249919e39f50f8665800e36449
amartushy <55608214+amartushy@users.noreply.github.com>
2020-02-09 16:59:36 -0800

    Update and rename loadcoursesandprofile2-3-3.js to loadcoursesandprofile2-3-4.js

--- loadcoursesandprofile2-3-4.js
+++ loadcoursesandprofile2-3-4.js
@@ -232,7 +232,7 @@ function loadTutorProfile(userId) {
 	}
 	sessionsLabel.innerHTML = sessionCount
 	averageLabel.innerHTML = (averageScore/averageCount).toFixed(2)
-	hourlyLabel.innerHTML = "$" + parseFloat(snapshot.child("/PPH/"))
+	hourlyLabel.innerHTML = "$" + snapshot.child("/PPH/")
 	
 	
 		
