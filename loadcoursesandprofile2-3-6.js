1e4c76c8509cec79380394bd071d944d0d16a7af
amartushy <55608214+amartushy@users.noreply.github.com>
2020-02-09 18:56:10 -0800

    Update loadcoursesandprofile2-3-6.js

--- loadcoursesandprofile2-3-6.js
+++ loadcoursesandprofile2-3-6.js
@@ -232,7 +232,7 @@ function loadTutorProfile(userId) {
 	}
 	sessionsLabel.innerHTML = sessionCount
 	averageLabel.innerHTML = (averageScore/averageCount).toFixed(2)
-	hourlyLabel.innerHTML = "$" + tutorsRateField.value
+	hourlyLabel.innerHTML = "$" + parseFloat(tutorsRateField.value)*2
 	
 	
 		
