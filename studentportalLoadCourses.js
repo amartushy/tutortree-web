a09fde028c34efcdc1225d0c64f6afff205658c4
amartushy <55608214+amartushy@users.noreply.github.com>
2020-04-06 19:52:30 -0700

    Update studentportalLoadCourses.js

--- studentportalLoadCourses.js
+++ studentportalLoadCourses.js
@@ -2,7 +2,18 @@ var courseRef = dataRef.parent
 
 function loadStudentPortal(studentId) {
       courseRef.once("value", function(snapshot) {
-    
+      //set which university student is at
+      var studentsEmail = snapshot.child("/users/" + studentId +/"email/").val()
+      var studentsUniversity = ""
+      
+      var universityDict = {
+      
+                                         
+                                         
+                                     
+                                    
+                                         }
+                                         
     
       })
 
