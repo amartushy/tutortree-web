30a6ef526e9c0539aa0fd5d860f38c8213452f41
amartushy <55608214+amartushy@users.noreply.github.com>
2020-04-08 13:35:04 -0700

    Update and rename studentportalLoadCourses1-2-6.js to studentportalLoadCourses1-2-7.js

--- studentportalLoadCourses1-2-7.js
+++ studentportalLoadCourses1-2-7.js
@@ -519,14 +519,16 @@ function checkoutWithNonceAndAmount(nonce, amount) {
         var xhttp = new XMLHttpRequest();
         var herokuURL = "https://tutortree-development.herokuapp.com/checkoutWithNonceAndAmount/"+nonce+"/"+amount
 				
+				/*
 				xhttp.onreadystatechange = function() {
 						if (xhttp.readyState == XMLHttpRequest.DONE) {
 								var response = xhttp.responseText
 						}
-				}
+				} */
+				
         xhttp.open("GET", herokuURL, true);
         xhttp.send();
-				console.log(response)
+				console.log(xhttp.response)
 }
 
 //HELPER FUNCTION TO CONVERT TIME
