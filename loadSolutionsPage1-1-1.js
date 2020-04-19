aa61a1b4f2fce6daa74662b553ca4a99a820d764
amartushy <55608214+amartushy@users.noreply.github.com>
2020-04-19 12:14:26 -0700

    Update and rename loadSolutionsPage.js to loadSolutionsPage1-1-1.js

--- loadSolutionsPage1-1-1.js
+++ loadSolutionsPage1-1-1.js
@@ -1,3 +1,77 @@
+//UPLOAD NEW SOLUTION REQUEST MODAL
+function loadNewSolutionModal(usersId) {
+
+      var storageRef = storageService.ref();
+			document.getElementById("new-question-modal-wrapper").style.display = "flex"
+      
+
+      var newSolutionSubject = document.getElementById("new-solution-subject")
+      var newSolutionCourse = document.getElementById("new-solution-course")
+
+      var newSolutionPhoto = document.getElementById("new-solution-photo")
+      newSolutionPhoto.addEventListener('click', openNewSolutionDialog)
+      var hiddenNewSolutionButton = document.getElementById("new-solution-select")
+      hiddenNewSolutionButton.addEventListener('change', handleNewSolutionUploadChange);
+
+
+      function openNewSolutionDialog() {
+            hiddenNewSolutionButton.click();
+      }
+
+      var newSolutionPreviewFile;
+      function handleNewSolutionUploadChange(e) {
+           newSolutionPreviewFile = e.target.files[0];
+           document.getElementById("new-solution-preview").innerHTML = newSolutionPreviewFile.name
+      }
+
+      document.getElementById('new-solution-upload').addEventListener('click', handleNewSolutionUpload);
+      async function handleNewSolutionUpload(e) {
+           const uploadTask = await storageRef.child(`problems/${newSolutionPreviewFile.name}`).put(newSolutionPreviewFile);
+           uploadNewSolutionRequest()
+      }
+
+      //Final Submit Button and Update Firebase
+      async function uploadNewSolutionRequest() {
+           var problemFileURL = ""
+           await storageRef.child('/problems/'+newSolutionPreviewFile.name).getDownloadURL().then(function(url) {
+                  problemFileURL = url.toString()
+           })
+           var dictToUpdate = {}
+           var newProblemDict = {
+           						"title" : document.getElementById("new-solution-title").value,
+                      "problemText" : document.getElementById("new-solution-text").value,
+                      "problemURL" : problemFileURL,
+                      "approved" : 0,
+                      "solution" : 0,
+                      "metadata" : {
+                          "uploadedBy" : usersID,
+                          "dateUploaded" : Math.round((new Date()).getTime() / 1000),
+                          "views" : 0,
+                          "upvotes" : 0,
+                       		}
+          } 
+          var problemID = create8CharID()
+           dictToUpdate[problemID] = newProblemDict
+                console.log(dictToUpdate)
+                solutionsRef.child("/"+newSolutionSubject.value+"/"+newSolutionCourse.value+"/").update(dictToUpdate)
+                sendSolutionRequestEmail
+
+     }
+}
+
+//Send email to info@ when new solution is requested
+function sendSolutionRequestEmail(problemId) {
+    var xhttp = new XMLHttpRequest();
+    const message  = "Problem ID: " +problemId
+    var title = "New Solution Request"
+    var herokuURL = "https://tutortree-development.herokuapp.com/sendEmailTo/info@jointutortree.com/"+title+"/"+message
+    console.log(herokuURL)
+		xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
+
+
+//UPLOAD SOLUTION FROM TUTORS END
 function loadUploadModal(tutorsID) {
 		var storageRef = storageService.ref();
     
