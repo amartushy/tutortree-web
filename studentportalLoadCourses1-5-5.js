cd2c719e71a841342a6d5ab5375f3d99c825da46
amartushy <55608214+amartushy@users.noreply.github.com>
2020-05-27 13:10:05 -0700

    Update and rename studentportalLoadCourses1-5-4.js to studentportalLoadCourses1-5-5.js

--- studentportalLoadCourses1-5-5.js
+++ studentportalLoadCourses1-5-5.js
@@ -354,7 +354,7 @@ function loadCheckoutModal(tutorsId, studentsId, tutorsName, tutorsImage, day, b
 			
 			var whichButton = document.getElementById(day+"-day-choice").children[2].id
 			var checkoutTime = document.querySelector(".checkout-time")
-			var start = getSecondsFromEpoch(whichButton) + timeSlot*1800
+			var start = getSecondsFromEpoch(whichButton)/1000 + timeSlot*1800
 			checkoutTime.setAttribute("id", start)
 			var timeDict = ["6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM",
 											"9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM",
