882e1e631fc4d8fb38b0dd0fd62f971a428857c5
amartushy <55608214+amartushy@users.noreply.github.com>
2020-07-18 15:07:35 -0700

    Update TutorOnboardingAdmin1-1-1.js

--- TutorOnboardingAdmin1-1-1.js
+++ TutorOnboardingAdmin1-1-1.js
@@ -1,14 +1,17 @@
 firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-	        var userDB = firebase.firestore()
-	        var userID = user.uid
-          
-          userDB.collection("users").doc(userID)
-    		      .onSnapshot(function(doc) {
-                  console.log("yay")
-              }
+	if (user) {
+		var userDB = firebase.firestore()
+		var userID = user.uid
 
-      } else {
-		      location.href = "https://www.jointutortree.com"
-      }
+		userDB.collection("users").onSnapshot(function(doc) {
+			if(doc.get(userID+'.admin') == true) {
+				console.log('yay')
+			} else {
+				location.href = "https://www.jointutortree.com"
+			}
+		}
+
+	} else {
+		location.href = "https://www.jointutortree.com"
+	}
 }
