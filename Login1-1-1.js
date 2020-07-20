3510731d255caff84bb57bebbbf13fa18d1e4405
amartushy <55608214+amartushy@users.noreply.github.com>
2020-07-19 23:24:39 -0700

    Update and rename Login.js to Login1-1-1.js

---
+++ Login1-1-1.js
@@ -0,0 +1,67 @@
+signInButton = document.getElementById("signIn");
+signInButton.addEventListener("click", signUp);
+passwordReset = document.getElementById("password-reset")
+passwordReset.addEventListener("click", sendPasswordReset)
+
+function sendPasswordReset() {
+
+	var auth = firebase.auth();
+	var emailAddress = document.getElementById('userEmail').value
+
+	auth.sendPasswordResetEmail(emailAddress).then(function() {
+		// Email sent.
+		alert("Thank you! A password reset link has been sent to " + emailAddress)
+	}).catch(function(error) {
+		// An error happened.
+		console.log("Wrong email or an error occurred")
+	});
+}
+
+
+function signUp(){
+	userEmail = document.getElementById('userEmail').value
+	userPassword = document.getElementById('userPassword').value
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then( function() {
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				userDB = firebase.firestore()
+				var userID = user.uid
+				
+				userDB.collection("users").doc(userID).get().then(function(doc) {		
+					if(doc.data().admin) {
+						document.getElementById('admin-nav-panel').style.display = 'flex'
+						document.getElementById('login-section').style.display = 'none'
+						
+						//admin navigation
+						document.getElementById('admin-nav').addEventListener('click', function() {
+							location.href = 'https://www.jointutortree.com/admin'
+						})
+
+						//tutor navigation
+						document.getElementById('applicants-nav').addEventListener('click', function() {
+							location.href = 'https://www.jointutortree.com/tutor/tutor-admin'
+						})
+
+						//metrics navigation
+						document.getElementById('applicants-nav').addEventListener('click', function() {
+							alert('Not available yet, sorry Jack :(')
+						})
+					} else if (doc.data().tutorApplicant) {
+						location.href = 'https://www.jointutortree.com/tutor/onboarding-dashboard'	
+					} else if (doc.data().tutorApplicationApproved) {
+						location.href = 'https://www.jointutortree.com/tutor/tutor-portal'
+					}
+											   
+				})
+			} else {
+				console.log("no user logged in")
+			}
+		});
+	}).catch(function(error){
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log("errorCode: " + errorCode +"\n"+ "errorMessage: " + errorMessage)
+		alert("Sorry, your username or password is incorrect")
+	});
+}
