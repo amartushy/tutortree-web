c8e41b34ca19ccd0a41305668f8e4f50f7e3244b
amartushy <55608214+amartushy@users.noreply.github.com>
2020-08-28 15:35:58 -0700

    Update and rename AmbassadorApplication1-3-4.js to AmbassadorApplication1-3-5.js

--- AmbassadorApplication1-3-5.js
+++ AmbassadorApplication1-3-5.js
@@ -46,13 +46,93 @@ almostBackButton.setAttribute('onClick', 'showForm(2)')
 
 function showForm(formInt) {
 	var formsArray = [basicInfoForm, aboutYouForm, socialMediaForm, almostDoneForm]
+	var checkFieldsArray = [checkBasic(), checkAbout(), checkSocial(), checkDone()]
 	
 	for (i=0; i < formsArray.length; i++) {
-		if (i == formInt) {
-			formsArray[i].style.display = "flex"
-		} else {
-			formsArray[i].style.display = "none"
+		
+		if(checkFieldsArray[i]) {
+			if (i == formInt) {
+				formsArray[i].style.display = "flex"
+			} else {
+				formsArray[i].style.display = "none"
+			}
 		}
+	}
+}
+
+func checkBasic() {
+	if (emailField.value = "") {
+		alert("Please enter your email")
+		return false
+	} else if (firstNameField.value = "") {
+		alert("Please enter your first name")
+		return false
+	} else if (lastNameField.value = "") {
+		alert("Please enter your last name")
+		return false
+	} else if (phoneNumberField.value = "") {
+		alert("Please enter your phone number")
+		return false
+	} else if (passwordField.value = "") {
+		alert("Please enter a good password")
+		return false
+	} else if (schoolField.value = "") {
+		alert("Please select which school you go to")
+		return false
+	} else if (yearField.value = "") {
+		alert("Please select what year you are in")
+		return false
+	} else {
+		return true
+	}
+}
+
+func checkAbout() {
+	if (majorField.value = "") {
+		alert("Please enter your major")
+		return false
+	} else if (nearCampusField.value = "") {
+		alert("Please enter whether you're near campus")
+		return false
+	} else if (organizationsField.value = "") {
+		alert("Please detail whether you're in any organizations")
+		return false
+	} else if (offCampusField.value = "") {
+		alert("Please enter your favorite place to go off campus")
+		return false
+	} else {
+		return	
+		return true
+	}
+}
+
+func checkSocial() {
+	if (socialMediaField.value = "") {
+		alert("Please enter you social media")
+		return false
+	} else if (followersField.value = "") {
+		alert("Please enter the number of followers")
+		return false
+	} else if (socialHandleField.value = "") {
+		alert("Please enter your social media link")
+		return false
+	} else if (ambassadorsMeme = "") {
+		alert("Please upload a meme")
+		return false
+	} else {
+		return	true
+	}
+}
+
+func checkDone() {
+	if (knowEmployeesField.value = "") {
+		alert("Please enter whether you know any TT employees")
+		return false
+	} else if (howHeardField.value = "") {
+		alert("Please select how you heard about us")
+		return false
+	} else {
+		return	true
 	}
 }
 
@@ -90,35 +170,55 @@ function createAmbassadorApplicant() {
 	})	
 }
 
-function createFirestoreAmbassadorApplicant(ambassadorID) {
-	var ambassadorToUpdate = {}
-              	var newAmbassadorDict = {
-                  	"email" : emailField.value,
-                  	"firstName" : firstNameField.value,
-                  	"lastName" : lastNameField.value,
-                  	"phoneNumber" : phoneNumberField.value,
-			"school" : schoolField.value,
-			"year" : yearField.value,
-			"major" : majorField.value,
-			"livesNearCampus" : nearCampusField.value,
-			"organizations" : organizationsField.value,
-			"favoriteOffCampus" : offCampusField.value,
-			"socialMediaField" : socialMediaField.value,
-			"numberOfFollowers" : followersField.value,
-			"socialHandle" : socialHandleField.value,
-			"memeURL" : ambassadorsMeme,
-			"knowsEmployees" : knowEmployeesField.value,
-			"howHeardField" : howHeardField.value,
-                  
-			"status" : "pending",
-			"metadata" : {
-				"dateApplied" : new Date / 1000
-				}
-              	}
-              ambassadorToUpdate[ambassadorID] = newAmbassadorDict
-              
-              ambassadorRef.collection("ambassadors").doc(ambassadorID).set(newAmbassadorDict)
-              .then(function() {
+function createNewUserAmbassadorApplicant(ambassadorID) {
+	var ambassadorApplication = {
+		"favoriteOffCampus" : offCampusField.value,
+		"howHeardField" : howHeardField.value,
+		"knowsEmployees" : knowEmployeesField.value,
+		"firstName" : firstNameField.value,
+		"lastName" : lastNameField.value,
+		"livesNearCampus" : nearCampusField.value,
+		"memeURL" : ambassadorsMeme,
+		"dateApplied" : new Date / 1000,
+		"numberOfFollowers" : followersField.value,
+		"organizations" : organizationsField.value,
+		"socialHandle" : socialMediaField.value,
+		"year" : yearField.value
+	}
+
+	var appUserInfo = {
+		"agreedTOS" : true,
+		"availability" : {
+			"Monday" : 0,
+			"Tuesday" : 0,
+			"Wednesday" : 0,
+			"Thursday" : 0,
+			"Friday" : 0,
+			"Saturday" : 0,
+			"Sunday" : 0
+		},
+		"bio" : "Please enter a short description of yourself",
+		"email" : emailField.value,
+		"isAdmin" : false,
+		"isEmailOn" : false,
+		"isPushOn" : false,
+		"isSMSOn" : false,
+		"isTutor" : false,
+		"major" : majorField.value,
+		"maxHPW" : 20,
+		"name" : firstNameField.value + " " + lastNameField,
+		"phoneNumber" : phoneNumberField.value,
+		"pricePHH" : 10,
+		"profileImage" : "https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2FTTLogo-Mint.png?alt=media&token=c8d6559e-7fa8-4fed-81cc-1f09d5da56a5",
+		"pushToken" : "",
+		"school" : schoolField.value,
+		"ambassadorApplicationStatus" : "pending"
+	}
+	
+	userDB.collection("ambassadorTest").doc(ambassadorID).set(appUserInfo, {merge: true})
+	.then(function() {
+			userDB.collection("ambassadorTest").doc(ambassadorID).collection("ambassadorApplication").doc("application").set(ambassadorApplication, { merge: true })
+
 			console.log("Ambassador application successfully submitted");
 			document.getElementById('ambassador-thanks').style.display = "flex"
 			document.getElementById('ambassador-submit-form').style.display = "none"
@@ -129,6 +229,47 @@ function createFirestoreAmbassadorApplicant(ambassadorID) {
 			sendSMSTo("4582108156", newAmbassadorMessage)
 		        sendSMSTo("5417311987", newAmbassadorMessage)
               })
+
+	
+}
+
+function createFirestoreAmbassadorApplicant(ambassadorID) {
+	var ambassadorApplication = {
+		"favoriteOffCampus" : offCampusField.value,
+		"howHeardField" : howHeardField.value,
+		"knowsEmployees" : knowEmployeesField.value,
+		"firstName" : firstNameField.value,
+		"lastName" : lastNameField.value,
+		"livesNearCampus" : nearCampusField.value,
+		"memeURL" : ambassadorsMeme,
+		"dateApplied" : new Date / 1000,
+		"numberOfFollowers" : followersField.value,
+		"organizations" : organizationsField.value,
+		"socialHandle" : socialMediaField.value,
+		"year" : yearField.value
+	}
+	
+	var existingUserInfo = {
+		"major" : majorField.value,
+		"name" : firstNameField.value + " " + lastNameField.value,
+		"phoneNumber" : phoneNumberField.value,
+		"ambassadorApplicationStatus" : "pending"
+		
+	}
+	userDB.collection("ambassadorTest").doc(ambassadorID).set(existingUserInfo, {merge : true})
+	
+	userDB.collection("ambassadorTest").doc(ambassadorID).collection("ambassadorApplication").doc("application").set(ambassadorApplication, { merge: true })
+	.then(function() {
+		console.log("Ambassador application successfully submitted");
+		document.getElementById('ambassador-thanks').style.display = "flex"
+		document.getElementById('ambassador-submit-form').style.display = "none"
+
+		ambassadorAccountCreated(newAmbassadorDict)
+
+		var newAmbassadorMessage = "New Ambassador Application : " + firstNameField.value + " has submitted an application to be a TutorTree ambassador. Their email is " + emailField.value
+		sendSMSTo("4582108156", newAmbassadorMessage)
+		sendSMSTo("5417311987", newAmbassadorMessage)
+	})
 }
 
 
