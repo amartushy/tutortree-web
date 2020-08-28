91d116ac88c610e9386e907be430c9816b90f74d
amartushy <55608214+amartushy@users.noreply.github.com>
2020-08-28 13:00:46 -0700

    Update and rename DATABASEMERGE1-4-9.js to DATABASEMERGE1-5-1.js

--- DATABASEMERGE1-5-1.js
+++ DATABASEMERGE1-5-1.js
@@ -74,8 +74,8 @@
 					"ambassadorApplicationStatus" : doc.data().status
 				}
 				
-				userDB.collection("ambassadorTest").doc(doc.id).set(appUserInfo, { merge: true })
-				userDB.collection("ambassadorTest").doc(doc.id).collection("ambassadorApplication").doc("application").set(ambassadorApplication, { merge: true })
+				userDB.collection("testImport").doc(doc.id).set(appUserInfo, { merge: true })
+				userDB.collection("testImport").doc(doc.id).collection("ambassadorApplication").doc("application").set(ambassadorApplication, { merge: true })
 			})
 		})
 	}
