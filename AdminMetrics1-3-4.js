e704461056c36df959018a886cd7b94915d9b0ae
amartushy <55608214+amartushy@users.noreply.github.com>
2020-09-07 14:42:14 -0700

    Update and rename AdminMetrics1-3-3.js to AdminMetrics1-3-4.js

--- AdminMetrics1-3-4.js
+++ AdminMetrics1-3-4.js
@@ -43,7 +43,7 @@ function showTTMetrics() {
         while(subjectArea.firstChild) {
             subjectArea.removeChild(subjectArea.firstChild)
         }
-        console.log(school.data())
+        console.log(school.length)
         school.forEach(function(doc) {
             var schoolTitle = doc.data().title
             var schoolPath = doc.id
