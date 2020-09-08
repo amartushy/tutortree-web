0272d071f1a7837ebad375aa64c86de88e0b0571
amartushy <55608214+amartushy@users.noreply.github.com>
2020-09-07 22:23:22 -0700

    Update and rename AdminTutor1-2-3.js to AdminTutor1-2-4.js

--- AdminTutor1-2-4.js
+++ AdminTutor1-2-4.js
@@ -45,6 +45,8 @@ firebase.auth().onAuthStateChanged(function(user) {
 		 
 		//Check if user is admin, else redirect: TODO
 		
+		//Load all applicants with no filters applied
+		tutortreeApplicants()
 		
 	//If user is not logged in (or an Admin) return them home
 	} else {
