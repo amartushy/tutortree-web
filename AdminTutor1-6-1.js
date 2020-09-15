7cf640e221ad004e9fa3ed10b48755f547f9de2d
amartushy <55608214+amartushy@users.noreply.github.com>
2020-09-14 18:43:10 -0700

    Update and rename AdminTutor1-5-9.js to AdminTutor1-6-1.js

--- AdminTutor1-6-1.js
+++ AdminTutor1-6-1.js
@@ -615,7 +615,7 @@ function updateInterviewPoints(pathString, incrementor) {
 		.then(function(doc) {
 			valueToUpdate = parseInt(doc.data().interviewScores.pathString) + incrementor
 			//Update score
-			console.log(valueToUpdate)
+			console.log(parseInt(doc.data().interviewScores.pathString))
 			userDB.collection("userTest")
 				.doc(globalApplicantID)
 				.collection("tutorApplication")
