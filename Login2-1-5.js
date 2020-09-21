431648a5c097476308028fd421da91e33d61dbd5
amartushy <55608214+amartushy@users.noreply.github.com>
2020-09-21 12:42:14 -0700

    Update and rename Login2-1-4.js to Login2-1-5.js

--- Login2-1-5.js
+++ Login2-1-5.js
@@ -1,16 +1,17 @@
 signInButton = document.getElementById("signIn");
 signInButton.addEventListener("click", signUp);
-passwordReset = document.getElementById("password-reset")
+passwordReset = document.getElementById("reset-password")
 passwordReset.addEventListener("click", sendPasswordReset)
 
 function sendPasswordReset() {
 
 	var auth = firebase.auth();
-	var emailAddress = document.getElementById('userEmail').value
+	var emailAddress = document.getElementById('reset-email').value
 
 	auth.sendPasswordResetEmail(emailAddress).then(function() {
 		// Email sent.
 		alert("Thank you! A password reset link has been sent to " + emailAddress)
+		document.getElementById('password-reset-modal').style.display = 'none'
 	}).catch(function(error) {
 		// An error happened.
 		console.log("Wrong email or an error occurred")
