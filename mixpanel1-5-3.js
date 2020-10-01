24219829815ec794c120299dcf0eeeccca03474d
amartushy <55608214+amartushy@users.noreply.github.com>
2020-09-30 18:02:13 -0700

    Update and rename mixpanel1-5-2.js to mixpanel1-5-3.js

--- mixpanel1-5-3.js
+++ mixpanel1-5-3.js
@@ -27,16 +27,13 @@ function mpTutorSignupPageViewed(){
 
 function mpTutorSignUp(user){
 	//adding super property here to always track School and reference
-	//mixpanel.register({
-	//	"School Name": user.school,
-	//	"How Heard": user.howHeard
-	//});
-	
-	//tracks the event of tutor signing up
-	mixpanel.track("Tutor Sign Up",{
+	mixpanel.register({
 		"School Name": user.school,
 		"How Heard": user.howHeard
 	});
+	
+	//tracks the event of tutor signing up
+	mixpanel.track("Tutor Sign Up")
 	//
 	mixpanel.people.set({
 		"School Name": user.school,
