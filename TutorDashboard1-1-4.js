fb34aafb914cc5bb2d8e55bbcaeea53d9a193d7f
amartushy <55608214+amartushy@users.noreply.github.com>
2020-10-05 17:34:02 -0700

    Update and rename TutorDashboard1-1-3.js to TutorDashboard1-1-4.js

--- TutorDashboard1-1-4.js
+++ TutorDashboard1-1-4.js
@@ -311,56 +311,6 @@ async function getCountOfSessions(ID) {
 }
 
 
-//Profile Functions_______________________________________________________________________________________________
-//Profile functions_____________________________________________________________________________
-storageRef = storageService.ref()
-
-function loadProfile() {
-
-    userDB.collection('userTest').doc(globalTutorID).get().then(async function(doc) {
-        var tutorData = doc.data()
-        var tutorsRating = await getRatingForUser(globalTutorID)
-        var sessionsCount = await getCountOfSessions(globalTutorID)
-
-        //create Photo element because setting src doesn't work :/
-        while(profileImageBlock.firstChild) {
-			profileImageBlock.removeChild(profileImageBlock.firstChild)
-        }
-        var tutorsProfilePhoto = document.createElement('img') 
-        tutorsProfilePhoto.setAttribute('class', 'upload-image')
-        tutorsProfilePhoto.src = tutorData.profileImage
-        tutorsProfilePhoto.addEventListener('click', function() {
-            uploadProfileImage()
-            updateProfileBlock.style.display = 'flex'
-        })
-        profileImageBlock.appendChild(tutorsProfilePhoto)
-
-        tutorsName.innerHTML = tutorData.name
-        sessionsText.innerHTML = sessionsCount
-        averageText.innerHTML = tutorsRating
-        hourlyText.innerHTML = '$' + tutorData.pricePHH * 2
-        tutorsBio.value = tutorData.bio
-
-        tutorsBio.onfocus = function() {
-            updateProfileBlock.style.display = 'flex'
-        }
-
-        tutorsName.onfocus = function() {
-            updateProfileBlock.style.display = 'flex'
-        }
-
-    })
-
-    updateProfile.addEventListener('click', function() {
-        //Update Profile Image
-
-        //Update Name
-
-        //Update Bio
-                
-    })
-}
-
 //Profile functions_____________________________________________________________________________
 
 function loadProfile() {
