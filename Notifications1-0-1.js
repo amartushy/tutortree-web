30e2d133f72a8002df44b29c39b222301852928f
amartushy <55608214+amartushy@users.noreply.github.com>
2020-10-11 12:34:04 -0700

    Update and rename Notifications1-0-0.js to Notifications1-0-1.js

--- Notifications1-0-1.js
+++ Notifications1-0-1.js
@@ -1,19 +1,19 @@
 //NOTIFICATIONS PUSH FUNCTIONS
 function sendSMSTo(number, message) {
 		var xhttp = new XMLHttpRequest();
-    var herokuURL = "https://tutortree-development.herokuapp.com/sendSMSTo/+1"+number+"/"+message
+    var herokuURL = "https://tutortree2.herokuapp.com/sendSMSTo/+1"+number+"/"+message
 		xhttp.open("GET", herokuURL, true);
 		xhttp.send();
 }
 function sendPushTo(token, title, message) {
 		var xhttp = new XMLHttpRequest();
-    var herokuURL = "https://tutortree-development.herokuapp.com/sendNotification/"+title+"/"+message+"/"+token+"/1"
+    var herokuURL = "https://tutortree2.herokuapp.com/sendNotification/"+title+"/"+message+"/"+token+"/1"
     xhttp.open("GET", herokuURL, true);
 		xhttp.send();
 }
 function sendEmailTo(email, title, message) {
 		var xhttp = new XMLHttpRequest();
-    var herokuURL = "https://tutortree-development.herokuapp.com/sendEmailTo/"+email+"/"+title+"/"+message
+    var herokuURL = "https://tutortree2.herokuapp.com/sendEmailTo/"+email+"/"+title+"/"+message
     console.log(herokuURL)
 		xhttp.open("GET", herokuURL, true);
 		xhttp.send();
