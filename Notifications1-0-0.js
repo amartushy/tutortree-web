edaad73c90c493ef75b7ccf5ff5b1956924a6240
amartushy <55608214+amartushy@users.noreply.github.com>
2020-10-11 12:01:05 -0700

    Create Notifications1-0-0.js

---
+++ Notifications1-0-0.js
@@ -0,0 +1,20 @@
+//NOTIFICATIONS PUSH FUNCTIONS
+function sendSMSTo(number, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree-development.herokuapp.com/sendSMSTo/+1"+number+"/"+message
+		xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
+function sendPushTo(token, title, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree-development.herokuapp.com/sendNotification/"+title+"/"+message+"/"+token+"/1"
+    xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
+function sendEmailTo(email, title, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree-development.herokuapp.com/sendEmailTo/"+email+"/"+title+"/"+message
+    console.log(herokuURL)
+		xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
