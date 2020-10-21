849fc0acff97b42425537ceae963603d24a1b699
amartushy <55608214+amartushy@users.noreply.github.com>
2020-10-21 12:53:50 -0700

    Create AccountCreation1-0-0.js

---
+++ AccountCreation1-0-0.js
@@ -0,0 +1,14 @@
+/* Elements needed:
+email
+school
+name
+major
+password
+phonenumber
+readTOS
+isSMSOn
+isEmailOn
+*/
+
+//Account creation needs to populate all CORE fields in the userTest collection
+//Check if account already exists, if not then create one and direct to confirmation page, if it does exist sign in and direct to confirmation page.
