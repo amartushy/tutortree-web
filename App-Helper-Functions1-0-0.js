3ac281af5ae4ed539d36f728c2020e082194f3fa
amartushy <55608214+amartushy@users.noreply.github.com>
2020-11-30 17:28:36 -0800

    Create App-Helper-Functions1-0-0.js

---
+++ App-Helper-Functions1-0-0.js
@@ -0,0 +1,40 @@
+//Helper Functions ___________________________________________________________________________________________________________
+async function getRatingForUser(ID) {
+    var rating = 0
+
+    await userDB.collection('userTest').doc(ID).collection('sessions').get().then(function(session) {
+        var ratingSum = 0
+        var sessionsWithRating = 0
+
+        session.forEach(function(doc) {
+            if(doc.data().ratingFromOtherUser != null) {
+                ratingSum += doc.data().ratingFromOtherUser
+                sessionsWithRating += 1
+                rating = ratingSum / sessionsWithRating
+            }
+        })
+    })
+
+    if (rating == 0) {
+        return ('NEW')
+    } else {
+        return rating.toFixed(2)
+    }
+}
+
+async function getCountOfSessions(ID) {
+    var sessions = 0
+
+    await userDB.collection('userTest').doc(ID).collection('sessions').get().then(function(session) {
+
+        session.forEach(function(doc) {
+            sessions += 1
+        })
+    })
+
+    if (sessions == 0) {
+        return ('NEW')
+    } else {
+        return sessions
+    }
+}
