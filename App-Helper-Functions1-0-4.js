b7fa098eb5b98c5f1e2691770e5cd91f5063a404
amartushy <55608214+amartushy@users.noreply.github.com>
2020-12-02 20:23:55 -0800

    Update App-Helper-Functions1-0-4.js

--- App-Helper-Functions1-0-4.js
+++ App-Helper-Functions1-0-4.js
@@ -107,3 +107,25 @@ function getFormattedTime(startEpoch, endEpoch) {
     var timeObject = [startString, endString]
     return(timeObject)
 }
+
+
+//NOTIFICATIONS PUSH FUNCTIONS
+function sendSMSTo(number, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree2.herokuapp.com/sendSMSTo/+1"+number+"/"+message
+		xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
+function sendPushTo(token, title, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree2.herokuapp.com/sendNotification/"+title+"/"+message+"/"+token+"/1"
+    xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
+function sendEmailTo(email, title, message) {
+		var xhttp = new XMLHttpRequest();
+    var herokuURL = "https://tutortree2.herokuapp.com/sendEmailTo/"+email+"/"+title+"/"+message
+    console.log(herokuURL)
+		xhttp.open("GET", herokuURL, true);
+		xhttp.send();
+}
