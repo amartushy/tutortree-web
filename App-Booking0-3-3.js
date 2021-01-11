1d8a676b8ce5f3156ee2e657761ac254086bdeb9
amartushy <55608214+amartushy@users.noreply.github.com>
2021-01-10 20:57:22 -0800

    Update and rename App-Booking0-3-2.js to App-Booking0-3-3.js

--- App-Booking0-3-3.js
+++ App-Booking0-3-3.js
@@ -616,16 +616,6 @@ function showErrorMessage(message) {
     $('#error-message').fadeIn().delay(5000).fadeOut("slow")
 }
 
-function createTransactionID(){
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 8; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
 function setProcessingInitialState() {
     $("#checkout-screen").hide( () => {
         $("#processing-screen").show()
