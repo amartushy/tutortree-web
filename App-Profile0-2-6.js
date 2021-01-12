519a68e9682cca7b3b3355121eea519a06abaf32
amartushy <55608214+amartushy@users.noreply.github.com>
2021-01-12 12:15:26 -0800

    Update and rename App-Profile0-2-5.js to App-Profile0-2-6.js

--- App-Profile0-2-6.js
+++ App-Profile0-2-6.js
@@ -1086,6 +1086,7 @@ function buildTimeSlot(startIndex, endIndex, day) {
     var timeSlotMinus = document.createElement('div')
     timeSlotMinus.setAttribute('class', 'timeslot-minus')
     timeSlotMinus.innerHTML = ''
+    timeSlotMinus.setAttribute('onClick', 'removeAvailability("'+day+'","'+startIndex+'","'+endIndex+'")')
     availabilityTimeDiv.appendChild(timeSlotMinus)
 
     var availabilityTime = document.createElement('div')
