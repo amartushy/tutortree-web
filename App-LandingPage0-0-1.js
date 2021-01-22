ce50fe69bbfab301b56ed4bc95d90965ee9f9451
amartushy <55608214+amartushy@users.noreply.github.com>
2021-01-21 20:38:11 -0800

    Update and rename App-LandingPage0-0-0.js to App-LandingPage0-0-1.js

---
+++ App-LandingPage0-0-1.js
@@ -0,0 +1,7 @@
+let tutorImageDictionary = {
+    'Abigail Cakebread' : 'https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2F8wn7tamCPSaGfwVC95Mkirt9zo52.jpeg?alt=media&token=ef469243-fbb2-4682-980b-1ab98784b308',
+    'Abigail Stevens' : 'https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2FDkc7rS1mQzZQeqFgeGQZnsVHzpa2.jpeg?alt=media&token=4653f9a2-2c73-4d34-a947-1db8d5ac2f1a',
+    'Aditi' : 'https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2FXCG0KcaDWhepj0bExm0ftbqUDQ02.jpeg?alt=media&token=d7caeb60-cb05-4a5b-b2bf-89d51d797b9c',
+    'Adrian': 'https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2F2lmqtqtpnqXZDfsRst9q4aekU952.jpeg?alt=media&token=c3798d3d-e648-4933-932a-da59604b567a',
+    'Adrian Martushev': 'https://firebasestorage.googleapis.com/v0/b/tutortree-68061.appspot.com/o/images%2FAdrian%20Martushev.jpg?alt=media&token=07f69496-f0a6-40cb-86eb-2f8c19d586e7'
+}
